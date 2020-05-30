3688164f2dc1ad10e36d707b47259bb9615d9d4f
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2020-05-30 19:22:37 -0300

    Adiciona um padronizador de código editorConfig, que padroniza o codigo entre as diversas plataformas que um usuário pode utilizar

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello gostack'});
-})
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
-})
+});
