f24e6ad2c3e23c5ad16b584628cf2a3d2451fd0e
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2020-05-30 19:45:38 -0300

    Add eslint at this code, allow all devs to have a pattern for write his code, following the guidelines from airbnb

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello gostack'});
-});
+app.get('/', (req, res) => res.json({ message: 'Hello gostack' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
